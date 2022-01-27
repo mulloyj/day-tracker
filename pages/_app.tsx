d41dc384978757aec0ac8e88128e28d41bce8456
Joshua Mulloy <josh@mulloy.ca>
2022-01-26 22:01:03 -0500

    cleaning stuff up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp() {
+  return <p>
+    This is me
+  </p>
 }
 
 export default MyApp
